bb8814d4e9d2b960812084914b2748e0341242b5
SebastianSpyrzewski <sebastian.spyrzewski@student.uj.edu.pl>
2024-10-17 21:54:18 +0200

    Fixed code format

--- src/main/kotlin/cacophony/regex/RegexStrings.kt
+++ src/main/kotlin/cacophony/regex/RegexStrings.kt
@@ -3,15 +3,15 @@ package cacophony.regex
 object RegexStrings {
     const val PUNCTUATION_REGEX = """\(|\)|[|]|->|=>|;|:|,"""
     const val KEYWORD_REGEX = """let|if|then|else|while|do|break|return"""
-    const val LITERAL_REGEX ="""-\d(\d)*|\d(\d)*|true|false"""
+    const val LITERAL_REGEX = """-\d(\d)*|\d(\d)*|true|false"""
     const val OPERATOR_REGEX = """==|=|<|>|<=|>=|+=|-=|+|-|\*|/|%|&&|\|\||!"""
     const val TYPE_IDENTIFIER_REGEX = """\u\w*"""
     const val VARIABLE_IDENTIFIER_REGEX = """(\l|_)\w*"""
     const val WHITESPACE_REGEX = """(\n|\r|\t| )*"""
 
-    const val COMMENT_REGEX = """#\N*""" //dont know iw we will implement it
+    const val COMMENT_REGEX = """#\N*""" // dont know iw we will implement it
 
-    //some auxiliary regexes:
+    // some auxiliary regexes:
     const val AUX_WORD_REGEX = """\d|\l|\u|_""" // \w
     const val AUX_DIGIT_REGEX = """0|1|2|3|4|5|6|7|8|9""" // \d
     const val AUX_LOWERCASE_REGEX = """a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|w|x|y|z""" // \l
