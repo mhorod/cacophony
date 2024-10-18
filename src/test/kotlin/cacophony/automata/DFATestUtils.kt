5f79074e4ed9594f46afbed14fe88b2a9d51f9c4
Michał Pajda <michal.pajda2002@gmail.com>
2024-10-19 00:11:41 +0200

    Formatted tests

--- src/test/kotlin/cacophony/automata/DFATestUtils.kt
+++ src/test/kotlin/cacophony/automata/DFATestUtils.kt
@@ -1,19 +1,31 @@
 package cacophony.automata
 
-fun createDFA(starting: Int, accepting: Set<Int>, productions: Map<Pair<Int, Char>, Int>): DFA<Int> {
+fun createDFA(
+    starting: Int,
+    accepting: Set<Int>,
+    productions: Map<Pair<Int, Char>, Int>,
+): DFA<Int> {
     val allStates =
-        setOf(starting).union(accepting).union(productions.keys.map { it.first }).union(productions.values)
+        setOf(starting)
+            .union(accepting)
+            .union(productions.keys.map { it.first })
+            .union(productions.values)
             .toList()
 
     return object : DFA<Int> {
         override fun getStartingState(): Int = starting
+
         override fun getAllStates(): List<Int> = allStates
+
         override fun getProductions(): Map<Pair<Int, Char>, Int> = productions
-        override fun getProduction(state: Int, symbol: Char): Int? = productions[state via symbol]
+
+        override fun getProduction(
+            state: Int,
+            symbol: Char,
+        ): Int? = productions[state via symbol]
+
         override fun isAccepting(state: Int): Boolean = accepting.contains(state)
     }
 }
 
-infix fun Int.via(label: Char): Pair<Int, Char> {
-    return Pair(this, label);
-}
+infix fun Int.via(label: Char): Pair<Int, Char> = Pair(this, label)
--- src/test/kotlin/cacophony/automata/minimalization/DFAMinimalizationTest.kt
+++ src/test/kotlin/cacophony/automata/minimalization/DFAMinimalizationTest.kt
@@ -1,20 +1,25 @@
 package cacophony.automata.minimalization
 
-import cacophony.automata.*
-import org.junit.jupiter.api.Assertions.*
+import cacophony.automata.areEquivalent
+import cacophony.automata.createDFA
+import cacophony.automata.via
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import kotlin.random.Random
 
 class DFAMinimalizationTest {
     @Test
-    fun `DFA with only accepting states is minimized to single state`() {
-        val dfa = createDFA(
-            0, setOf(0, 1, 2), mapOf(
-                0 via 'a' to 1,
-                1 via 'a' to 2,
-                2 via 'a' to 0,
+    fun `DFA accepting multiples of a is minimized to single state`() {
+        val dfa =
+            createDFA(
+                0,
+                setOf(0, 1, 2),
+                mapOf(
+                    0 via 'a' to 1,
+                    1 via 'a' to 2,
+                    2 via 'a' to 0,
+                ),
             )
-        )
         val minimized = dfa.minimalize()
         assertTrue(areEquivalent(dfa, minimized))
         assertEquals(1, minimized.getAllStates().size)
@@ -22,43 +27,54 @@ class DFAMinimalizationTest {
 
     @Test
     fun `DFA accepting a single letter is minimized to two states`() {
-        val dfa = createDFA(
-            0, setOf(1), mapOf(
-                0 via 'a' to 1,
-                1 via 'b' to 2,
-                2 via 'a' to 3,
-                3 via 'a' to 2,
+        val dfa =
+            createDFA(
+                0,
+                setOf(1),
+                mapOf(
+                    0 via 'a' to 1,
+                    1 via 'b' to 2,
+                    2 via 'a' to 3,
+                    3 via 'a' to 2,
+                ),
             )
-        )
         val minimized = dfa.minimalize()
         assertTrue(areEquivalent(dfa, minimized))
         assertEquals(2, minimized.getAllStates().size)
     }
 
     @Test
-    fun `DFA accepting a single 15-word is minimized to 16 states `() {
-        val dfa = createDFA(
-            0, setOf(15), mapOf(
-                0 via 'd' to 1,
-                1 via 'e' to 2,
-                2 via 'r' to 3,
-                3 via 'm' to 4,
-                4 via 'a' to 5,
-                5 via 't' to 6,
-                6 via 'o' to 7,
-                7 via 'g' to 8,
-                8 via 'l' to 9,
-                9 via 'y' to 10,
-                10 via 'p' to 11,
-                11 via 'h' to 12,
-                12 via 'i' to 13,
-                13 via 'c' to 14,
-                14 via 's' to 15,
-
-                16 via 'x' to 4, // unreachable state
-                7 via 'x' to 17, // dead state
+    fun `DFA accepting a single 15-word is minimized to 16 states`() {
+        val dfa =
+            createDFA(
+                0,
+                setOf(15),
+                mapOf(
+                    0 via 'd' to 1,
+                    1 via 'e' to 2,
+                    2 via 'r' to 3,
+                    3 via 'm' to 4,
+                    4 via 'a' to 5,
+                    5 via 't' to 6,
+                    6 via 'o' to 7,
+                    7 via 'g' to 8,
+                    8 via 'l' to 9,
+                    9 via 'y' to 10,
+                    10 via 'p' to 11,
+                    11 via 'h' to 12,
+                    12 via 'i' to 13,
+                    13 via 'c' to 14,
+                    14 via 's' to 15,
+                    // duplicated path
+                    7 via 'g' to 16,
+                    16 via 'l' to 17,
+                    17 via 'y' to 18,
+                    18 via 'p' to 19,
+                    19 via 'h' to 12,
+                    // unreachable state
+                    20 via 'x' to 7,
+                ),
             )
-        )
         val minimized = dfa.minimalize()
         assertTrue(areEquivalent(dfa, minimized))
         assertEquals(16, minimized.getAllStates().size)
