9e59f1acb18aa751e7b7b9ef4fce46b5294d5f0c
Kacper Topolski <kacpertopolski@op.pl>
2024-10-18 22:00:33 +0200

    Add Partition Refinement

---
+++ src/main/kotlin/cacophony/automata/minimalization/PartitionRefinement.kt
@@ -0,0 +1,47 @@
+package cacophony.automata.minimalization
+
+// Represents a set of the partition and allow for quick comparisons
+data class PartitionId(val id: Int)
+
+class PartitionRefinement<E>(baseSet: Collection<E>) {
+    private val elementToPartitionId: MutableMap<E, PartitionId> = HashMap()
+    private val partitionToElements: MutableMap<PartitionId, MutableSet<E>> = HashMap()
+
+    init {
+        baseSet.associateWithTo(elementToPartitionId) { PartitionId(0) }
+        partitionToElements[PartitionId(0)] = baseSet.toMutableSet()
+    }
+
+    // Refines the partition
+    // Returns a list of pairs (OldId, NewId), indicating that the OldId set has been split into two sets (OldId, NewId)
+    fun refine(refineBy: Collection<E>): List<Pair<PartitionId, PartitionId>> {
+        return refineBy
+            .groupBy(this::getPartitionId)
+            .map { (oldId, elements) ->
+                val oldPartition = partitionToElements[oldId]!!
+                val newPartition = elements.toMutableSet()
+                if (oldPartition.size == newPartition.size) return@map null
+
+                val newId = PartitionId(partitionToElements.size)
+
+                partitionToElements[newId] = newPartition
+                oldPartition.removeAll(newPartition)
+                for (e in newPartition) elementToPartitionId[e] = newId
+
+                return@map Pair(oldId, newId)
+            }
+            .filterNotNull()
+    }
+
+    fun getPartitionId(e: E): PartitionId {
+        return elementToPartitionId[e]!!
+    }
+
+    fun getElements(id: PartitionId): Set<E> {
+        return partitionToElements[id]!!
+    }
+
+    fun getAllPartitions(): Collection<Set<E>> {
+        return partitionToElements.values
+    }
+}
---
+++ src/test/kotlin/cacophony/automata/minimalization/PartitionRefinementTest.kt
@@ -0,0 +1,63 @@
+package cacophony.automata.minimalization
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+class PartitionRefinementTest {
+    @Test
+    fun refineTest1() {
+        val pr = PartitionRefinement(listOf(0, 1, 2, 3, 4))
+        pr.refine(listOf(0, 2, 3, 4))
+
+        val expected = setOf(setOf(0, 2, 3, 4), setOf(1))
+        val actual = pr.getAllPartitions().toSet()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun refineTest2() {
+        val pr = PartitionRefinement(listOf(0, 1, 2, 3, 4))
+        pr.refine(listOf(0, 2, 3, 4))
+        pr.refine(listOf(3))
+
+        val expected = setOf(setOf(0, 2, 4), setOf(1), setOf(3))
+        val actual = pr.getAllPartitions().toSet()
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun refineWithDuplicates() {
+        val pr = PartitionRefinement(listOf(0, 1))
+        val refinements = pr.refine(listOf(0, 0))
+        assertEquals(1, refinements.size)
+    }
+
+    private fun <E> refineOnce(partitions: Set<Set<E>>, refiningSet: Set<E>): Set<Set<E>> {
+        return partitions
+            .flatMap { listOf(it.intersect(refiningSet), it.subtract(refiningSet)) }
+            .filter { it.isNotEmpty() }
+            .toSet()
+    }
+
+    private fun getAllPartitionsLargeRandomOnce(n: Int, seed: Int) {
+        val random = Random(seed)
+        val base = (0..n).toSet()
+        val pr = PartitionRefinement(base)
+        var correct = setOf(base)
+
+        for (it in 0..20) {
+            assertEquals(correct, pr.getAllPartitions().toSet())
+
+            val refiningSet = base.filter { _ -> random.nextBoolean()}.toSet()
+            pr.refine(refiningSet)
+            correct = refineOnce(correct, refiningSet)
+        }
+    }
+
+    @Test
+    fun getAllPartitionsLargeRandom() {
+        (0..15).forEach { getAllPartitionsLargeRandomOnce(500, it) }
+        (0..150).forEach { getAllPartitionsLargeRandomOnce(25, it) }
+    }
+}
