3df23f64e08c21e598cd1f8efae18893c48ddb25
SebastianSpyrzewski <sebastian.spyrzewski@student.uj.edu.pl>
2024-10-18 17:21:21 +0200

    Added token category #2

--- src/test/kotlin/cacophony/regex/RegexStringsTest.kt
+++ src/test/kotlin/cacophony/regex/RegexStringsTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test
 
 internal class RegexStringsTest {
     @Test
-    fun testSpecialCharacterRegex()  {
+    fun testSpecialCharacterRegex() {
         val expected = """0|1|2|3|4|5|6|7|8|9"""
         assertEquals(expected, RegexStrings.getSpecialCharacterRegex('d'))
     }
