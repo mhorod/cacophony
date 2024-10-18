c0f1cc7414980eefa74027758c47798194244b58
SebastianSpyrzewski <sebastian.spyrzewski@student.uj.edu.pl>
2024-10-18 16:48:11 +0200

    Added token category #2

--- src/test/kotlin/cacophony/regex/RegexStringsTest.kt
+++ src/test/kotlin/cacophony/regex/RegexStringsTest.kt
@@ -7,15 +7,17 @@ import org.junit.jupiter.api.Test
 
 internal class RegexStringsTest {
     @Test
-    fun testSpecialCharacterRegex() {
+    fun testSpecialCharacterRegex()  {
         val expected = """0|1|2|3|4|5|6|7|8|9"""
         assertEquals(expected, RegexStrings.getSpecialCharacterRegex('d'))
     }
+
     @Test
     fun testGeneralCategoryRegex() {
         val expected = """let|if|then|else|while|do|break|return"""
         assertEquals(expected, RegexStrings.getCategoryRegex(TokenCategoryGeneral.KEYWORD))
     }
+
     @Test
     fun testSpecificCategoryRegex() {
         val expected = """\("""
