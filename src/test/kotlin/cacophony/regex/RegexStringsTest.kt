e3a7cf26fa0e36d2b7c96c9c4cf8aa6347d36317
SebastianSpyrzewski <sebastian.spyrzewski@student.uj.edu.pl>
2024-10-18 16:41:54 +0200

    Added token category #2

--- src/test/kotlin/cacophony/regex/RegexStringsTest.kt
+++ src/test/kotlin/cacophony/regex/RegexStringsTest.kt
@@ -2,22 +2,22 @@ package cacophony.regex
 
 import cacophony.token.TokenCategoryGeneral
 import cacophony.token.TokenCategorySpecific
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class RegexStringsTest{
+internal class RegexStringsTest {
     @Test
-    fun testSpecialCharacterRegex(){
+    fun testSpecialCharacterRegex() {
         val expected = """0|1|2|3|4|5|6|7|8|9"""
         assertEquals(expected, RegexStrings.getSpecialCharacterRegex('d'))
     }
     @Test
-    fun testGeneralCategoryRegex(){
+    fun testGeneralCategoryRegex() {
         val expected = """let|if|then|else|while|do|break|return"""
         assertEquals(expected, RegexStrings.getCategoryRegex(TokenCategoryGeneral.KEYWORD))
     }
     @Test
-    fun testSpecificCategoryRegex(){
+    fun testSpecificCategoryRegex() {
         val expected = """\("""
         assertEquals(expected, RegexStrings.getCategoryRegex(TokenCategorySpecific.LEFT_PARENTHESIS))
     }
