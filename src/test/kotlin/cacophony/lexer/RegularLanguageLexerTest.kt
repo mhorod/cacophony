8ce7ecc5661fb2cb8f76e6065802b8cc9f8d0b0b
katzper01 <katzper.michno@gmail.com>
2024-10-19 15:06:42 +0200

    add missing import

--- src/test/kotlin/cacophony/lexer/RegularLanguageLexerTest.kt
+++ src/test/kotlin/cacophony/lexer/RegularLanguageLexerTest.kt
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
 import org.mockito.kotlin.times
