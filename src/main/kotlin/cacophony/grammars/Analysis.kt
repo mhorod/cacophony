570c3ea9fb48aaa0d5a544b2f1bbbb4daa1f01f7
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-10-26 00:08:34 +0200

    Add grammar utils

--- src/main/kotlin/cacophony/grammars/Analysis.kt
+++ src/main/kotlin/cacophony/grammars/Analysis.kt
@@ -2,43 +2,45 @@ package cacophony.grammars
 
 import cacophony.automata.DFA
 
-typealias DFAStateReference<StateType, AtomType, ResultType> = Pair<StateType, DFA<StateType, AtomType, ResultType>>
+typealias DFAStateReference<StateType, SymbolType> = Pair<StateType, DFA<StateType, SymbolType, Production<SymbolType>>>
 
 fun <StateType, SymbolType> findNullable(
     automata: Map<SymbolType, DFA<StateType, SymbolType, Production<SymbolType>>>,
-): Collection<DFAStateReference<StateType, SymbolType, Production<SymbolType>>> {
+): Collection<DFAStateReference<StateType, SymbolType>> {
     TODO("Not implemented")
 }
 
 fun <StateType, SymbolType> findFirst(
     automata: Map<SymbolType, DFA<StateType, SymbolType, Production<SymbolType>>>,
-    nullable: Collection<DFAStateReference<StateType, SymbolType, Production<SymbolType>>>,
-): Map<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, Collection<SymbolType>> {
+    nullable: Collection<DFAStateReference<StateType, SymbolType>>,
+): Map<DFAStateReference<StateType, SymbolType>, Collection<SymbolType>> {
     TODO("Not implemented")
 }
 
 fun <StateType, SymbolType> findFollow(
     automata: Map<SymbolType, DFA<StateType, SymbolType, Production<SymbolType>>>,
-    nullable: Collection<DFAStateReference<StateType, SymbolType, Production<SymbolType>>>,
-    first: Map<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, Collection<SymbolType>>,
-): Map<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, Collection<SymbolType>> {
+    nullable: Collection<DFAStateReference<StateType, SymbolType>>,
+    first: Map<DFAStateReference<StateType, SymbolType>, Collection<SymbolType>>,
+): Map<DFAStateReference<StateType, SymbolType>, Collection<SymbolType>> {
     TODO("Not implemented")
 }
 
 data class AnalyzedGrammar<StateType, SymbolType>(
     val syncSymbolTypes: Collection<SymbolType>,
     val automata: Map<SymbolType, DFA<StateType, SymbolType, Production<SymbolType>>>,
+    val nullable: Collection<DFAStateReference<StateType, SymbolType>>,
+    val first: Map<DFAStateReference<StateType, SymbolType>, Collection<SymbolType>>,
+    val follow: Map<DFAStateReference<StateType, SymbolType>, Collection<SymbolType>>,
 ) {
-    val nullable: Collection<DFAStateReference<StateType, SymbolType, Production<SymbolType>>> = findNullable(automata)
-    val first: Map<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, Collection<SymbolType>> =
-        findFirst(
-            automata,
-            nullable,
-        )
-    val follow: Map<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, Collection<SymbolType>> =
-        findFollow(
-            automata,
-            nullable,
-            first,
-        )
+    companion object {
+        fun <StateType, SymbolType> fromAutomata(
+            syncSymbolTypes: Collection<SymbolType>,
+            automata: Map<SymbolType, DFA<StateType, SymbolType, Production<SymbolType>>>,
+        ): AnalyzedGrammar<StateType, SymbolType> {
+            val nullable = findNullable(automata)
+            val first = findFirst(automata, nullable)
+            val follow = findFollow(automata, nullable, first)
+            return AnalyzedGrammar(syncSymbolTypes, automata, nullable, first, follow)
+        }
+    }
 }
--- src/main/kotlin/cacophony/grammars/Grammar.kt
+++ src/main/kotlin/cacophony/grammars/Grammar.kt
@@ -1,5 +1,7 @@
 package cacophony.grammars
 
+import cacophony.automata.joinAutomata
+import cacophony.automata.minimalization.buildDFAFromRegex
 import cacophony.utils.AlgebraicRegex
 
 data class Production<SymbolType>(
@@ -7,7 +9,19 @@ data class Production<SymbolType>(
     val rhs: AlgebraicRegex<SymbolType>,
 )
 
+infix fun <SymbolType> SymbolType.produces(regex: AlgebraicRegex<SymbolType>) = Production(this, regex)
+
 data class Grammar<SymbolType>(
     val start: SymbolType,
     val productions: Collection<Production<SymbolType>>,
-)
+) {
+    fun buildAutomata() = productions.groupBy { it.lhs }.mapValues { buildAutomaton(it.value) }
+
+    private fun buildAutomaton(productions: List<Production<SymbolType>>) =
+        joinAutomata(productions.map { buildDFAFromRegex(it.rhs) }.zip(productions))
+}
+
+fun <SymbolType> grammarOf(
+    start: SymbolType,
+    vararg productions: Production<SymbolType>,
+) = Grammar(start, productions.toList())
