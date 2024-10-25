8aada05f76cbce87f4735985e16bb34f762e2d9b
Rafal Pyzik <rafal.pyzik@student.uj.edu.pl>
2024-10-25 22:07:47 +0200

    Initial version of the constructor

--- src/main/kotlin/cacophony/parser/LLOneParser.kt
+++ src/main/kotlin/cacophony/parser/LLOneParser.kt
@@ -1,15 +1,64 @@
 package cacophony.parser
 
 import cacophony.grammars.AnalyzedGrammar
+import cacophony.grammars.DFAStateReference
 import cacophony.grammars.ParseTree
+import cacophony.grammars.Production
 import cacophony.utils.Diagnostics
 
-class LLOneParser<SymbolType : Enum<SymbolType>> : Parser<SymbolType> {
+class LLOneParser<StateType, SymbolType : Enum<SymbolType>>(
+    private val nextAction: Map<SymbolType, MutableMap<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, SymbolType?>>,
+) : Parser<SymbolType> {
+
     companion object {
-        fun <StateType, SymbolType : Enum<SymbolType>> fromAnalyzedGrammar(
+        // Constructs LLOneParser with computed nextAction map.
+        // nextAction gives symbol on the production that we should use next
+        // depending on the current input symbol and dfa state.
+        // null denotes no suitable production, then we should accept.
+        // Throws if grammar is not LL(1).
+        inline fun <StateType, reified SymbolType : Enum<SymbolType>> fromAnalyzedGrammar(
             analyzedGrammar: AnalyzedGrammar<StateType, SymbolType>,
-        ): LLOneParser<SymbolType> {
-            TODO("Not yet implemented")
+        ): LLOneParser<StateType, SymbolType> {
+            val nextAction: Map<SymbolType, MutableMap<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, SymbolType?>> =
+                enumValues<SymbolType>().toList().associateWith { mutableMapOf() }
+
+            analyzedGrammar.automata.forEach { (dfaLabel, dfa) -> // automaton for the grammar symbol
+                dfa.getAllStates().forEach { curState -> // dfa state we are currently in
+                    val curStateRef = DFAStateReference(curState, dfa)
+
+                    enumValues<SymbolType>().forEach { inputSymbol -> // symbol from the input
+                        val acceptableSymbols = mutableListOf<SymbolType>()
+                        enumValues<SymbolType>().forEach { prodSymbol -> // symbol in the production
+                            val nextState = dfa.getProduction(curState, prodSymbol)
+                            if (nextState != null) {
+                                val nextStateRef = DFAStateReference(nextState, dfa)
+                                if (analyzedGrammar.first[nextStateRef]?.contains(inputSymbol) == true) {
+                                    acceptableSymbols.add(prodSymbol)
+                                } else if (analyzedGrammar.nullable.contains(nextStateRef) and
+                                    (analyzedGrammar.follow[nextStateRef]?.contains(inputSymbol) == true)
+                                ) {
+                                    acceptableSymbols.add(prodSymbol)
+                                }
+                            }
+                        }
+
+                        if (acceptableSymbols.size > 1) { // too many productions are suitable
+                            throw ParserConstructorErrorException("Sorry, this is not an LL(1) grammar :c")
+                        }
+                        if (acceptableSymbols.size == 1) {
+                            nextAction[inputSymbol]!![curStateRef] = acceptableSymbols[0]
+                        } else { // acceptableSymbols.size == 0
+                            if (dfa.isAccepting(curState)) {
+                                nextAction[inputSymbol]!![curStateRef] = null
+                            } else { // nowhere to go and cannot accept
+                                throw ParserConstructorErrorException("Sorry, this is not an LL(1) grammar :c")
+                            }
+                        }
+                    }
+                }
+            }
+
+            return LLOneParser(nextAction)
         }
     }
 
--- src/main/kotlin/cacophony/parser/Parser.kt
+++ src/main/kotlin/cacophony/parser/Parser.kt
@@ -3,6 +3,8 @@ package cacophony.parser
 import cacophony.grammars.ParseTree
 import cacophony.utils.Diagnostics
 
+class ParserConstructorErrorException(reason: String) : Exception(reason)
+
 interface Parser<SymbolType : Enum<SymbolType>> {
     fun process(
         terminals: List<ParseTree.Leaf<SymbolType>>,
