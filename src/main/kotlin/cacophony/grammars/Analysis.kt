9123f36c62831356333236c52bcd9f4524c8904f
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-10-27 12:02:05 +0100

    Apply suggestins

--- src/main/kotlin/cacophony/grammars/Analysis.kt
+++ src/main/kotlin/cacophony/grammars/Analysis.kt
@@ -1,6 +1,8 @@
 package cacophony.grammars
 
 import cacophony.automata.DFA
+import cacophony.automata.joinAutomata
+import cacophony.automata.minimalization.buildDFAFromRegex
 
 typealias DFAStateReference<StateType, SymbolType, ResultType> =
     Pair<StateType, DFA<StateType, SymbolType, ResultType>>
@@ -30,21 +32,32 @@ fun <StateType, SymbolType, ResultType> findFollow(
 }
 
 data class AnalyzedGrammar<StateType, SymbolType>(
-    val syncSymbolTypes: Collection<SymbolType>,
+    val syncSymbols: Collection<SymbolType>,
     val automata: Map<SymbolType, DFA<StateType, SymbolType, Production<SymbolType>>>,
     val nullable: Collection<DFAStateReference<StateType, SymbolType, Production<SymbolType>>>,
     val first: StateToSymbolsMap<StateType, SymbolType, Production<SymbolType>>,
     val follow: StateToSymbolsMap<StateType, SymbolType, Production<SymbolType>>,
 ) {
     companion object {
-        fun <StateType, SymbolType, ResultType> fromAutomata(
-            syncSymbolTypes: Collection<SymbolType>,
+        fun <SymbolType> fromGrammar(
+            syncSymbols: Collection<SymbolType>,
+            grammar: Grammar<SymbolType>,
+        ) = fromAutomata(syncSymbols, buildAutomata(grammar))
+
+        fun <SymbolType> buildAutomata(grammar: Grammar<SymbolType>) =
+            grammar.productions.groupBy { it.lhs }.mapValues { buildAutomaton(it.value) }
+
+        private fun <SymbolType> buildAutomaton(productions: List<Production<SymbolType>>) =
+            joinAutomata(productions.map { buildDFAFromRegex(it.rhs) }.zip(productions))
+
+        private fun <StateType, SymbolType> fromAutomata(
+            syncSymbols: Collection<SymbolType>,
             automata: Map<SymbolType, DFA<StateType, SymbolType, Production<SymbolType>>>,
         ): AnalyzedGrammar<StateType, SymbolType> {
             val nullable = findNullable(automata)
             val first = findFirst(automata, nullable)
             val follow = findFollow(automata, nullable, first)
-            return AnalyzedGrammar(syncSymbolTypes, automata, nullable, first, follow)
+            return AnalyzedGrammar(syncSymbols, automata, nullable, first, follow)
         }
     }
 }
