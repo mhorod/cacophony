4dbb544eba484bd8f6b194bfaea1772eacc201d8
Rafal Pyzik <rafal.pyzik@student.uj.edu.pl>
2024-10-27 21:13:31 +0100

    Change of logic, many fixes, 2 more tests

--- src/main/kotlin/cacophony/parser/LLOneParser.kt
+++ src/main/kotlin/cacophony/parser/LLOneParser.kt
@@ -7,17 +7,24 @@ import cacophony.grammars.Production
 import cacophony.utils.Diagnostics
 
 class LLOneParser<StateType, SymbolType : Enum<SymbolType>>(
-    private val nextAction: Map<SymbolType, MutableMap<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, SymbolType?>>,
+    private val nextAction: Map<SymbolType, Map<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, SymbolType?>>,
 ) : Parser<SymbolType> {
     companion object {
         // Constructs LLOneParser with computed nextAction map.
         // nextAction gives symbol on the production that we should use next
         // depending on the current input symbol and dfa state.
-        // null denotes no suitable production, then we should accept.
+        // null denotes no suitable production, then we should try to accept.
         // Throws if grammar is not LL(1).
         inline fun <StateType, reified SymbolType : Enum<SymbolType>> fromAnalyzedGrammar(
             analyzedGrammar: AnalyzedGrammar<StateType, SymbolType>,
         ): LLOneParser<StateType, SymbolType> {
+            // Currently the logic is as follows:
+            // 1. Loop over all DFAStateReference and inputSymbol: SymbolType.
+            // 2. For each production with some symbol P, using nullable, first and follow on the start state of the DFA for P
+            //    find all suitable productions (if there is no DFA for P, meaning P is terminal, production is suitable iff P == inputSymbol).
+            // 3. If there are at least 2 suitable productions then our grammar is not LL(1).
+            // 4. If there is exactly one then ok.
+            // 5. If there are none, in process() we should try to accept in current DFA, else it is a parsing error.
             val nextAction: Map<SymbolType, MutableMap<DFAStateReference<StateType, SymbolType, Production<SymbolType>>, SymbolType?>> =
                 enumValues<SymbolType>().toList().associateWith { mutableMapOf() }
 
@@ -27,33 +34,40 @@ class LLOneParser<StateType, SymbolType : Enum<SymbolType>>(
 
                     enumValues<SymbolType>().forEach { inputSymbol -> // symbol from the input
                         val suitableSymbols = mutableListOf<SymbolType>()
-                        enumValues<SymbolType>().forEach { prodSymbol -> // symbol in the production
-                            val nextState = dfa.getProduction(curState, prodSymbol)
-                            if (nextState != null) {
-                                val nextStateRef = DFAStateReference(nextState, dfa)
-                                if (analyzedGrammar.first[nextStateRef]?.contains(inputSymbol) == true) {
+                        enumValues<SymbolType>().forEach prod@{ prodSymbol -> // symbol in the production
+                            dfa.getProduction(curState, prodSymbol) ?: return@prod
+
+                            val dfaForProdSymbol = analyzedGrammar.automata[prodSymbol]
+                            if (dfaForProdSymbol != null) { // nonterminal
+                                val nextState = dfaForProdSymbol.getStartingState()
+                                val nextStateRef = DFAStateReference(nextState, dfaForProdSymbol)
+                                if ((analyzedGrammar.first[nextStateRef]?.contains(inputSymbol) == true) or (prodSymbol == inputSymbol)) {
                                     suitableSymbols.add(prodSymbol)
                                 } else if (analyzedGrammar.nullable.contains(nextStateRef) and
                                     (analyzedGrammar.follow[nextStateRef]?.contains(inputSymbol) == true)
                                 ) {
                                     suitableSymbols.add(prodSymbol)
                                 }
+                            } else { // terminal
+                                if (prodSymbol == inputSymbol) {
+                                    suitableSymbols.add(prodSymbol)
+                                }
                             }
                         }
 
+                        // Leaving this for the convenience in (very) possible future debugging.
+//                        println("state $curState in $dfaLabel, possibilities for $inputSymbol are $suitableSymbols")
+
                         if (suitableSymbols.size > 1) { // too many productions are suitable
-                            // TODO: more expressive error
-                            throw ParserConstructorErrorException("Sorry, this is not an LL(1) grammar :c")
+                            throw ParserConstructorErrorException(
+                                "Not an LL(1) grammar: " +
+                                    "state $curState in dfa $dfaLabel, possible productions for $inputSymbol are $suitableSymbols",
+                            )
                         }
                         if (suitableSymbols.size == 1) {
                             nextAction[inputSymbol]!![curStateRef] = suitableSymbols[0]
                         } else { // acceptableSymbols.size == 0
-                            if (dfa.isAccepting(curState)) {
-                                nextAction[inputSymbol]!![curStateRef] = null
-                            } else { // nowhere to go and cannot accept
-                                // TODO: more expressive error
-                                throw ParserConstructorErrorException("Sorry, this is not an LL(1) grammar :c")
-                            }
+                            nextAction[inputSymbol]!![curStateRef] = null
                         }
                     }
                 }
--- src/test/kotlin/cacophony/parser/LLOneParserTest.kt
+++ src/test/kotlin/cacophony/parser/LLOneParserTest.kt
@@ -3,26 +3,78 @@ package cacophony.parser
 import cacophony.automata.SimpleDFA
 import cacophony.automata.minimalization.via
 import cacophony.grammars.AnalyzedGrammar
+import cacophony.grammars.DFAStateReference
 import cacophony.grammars.Production
 import cacophony.utils.AlgebraicRegex
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 class LLOneParserTest {
-    private fun <T> results(vararg results: T) = results.associateWith { }
-
-    private fun <StateType, AtomType> productions() = mapOf<Pair<StateType, AtomType>, StateType>()
-
     enum class Symbol {
+        // nonterminals
         A,
         B,
         C,
+
+        // terminals
+        X,
+        Y,
+    }
+
+    @Test
+    fun `constructor doesn't throw on a simple grammar`() {
+//        A -> B
+//        B -> C
+//        C -> X
+        val dfaA =
+            SimpleDFA(
+                0,
+                mapOf(0 via Symbol.B to 1),
+                mapOf(
+                    1 to Production(Symbol.A, AlgebraicRegex.atomic(Symbol.B)),
+                ),
+            )
+        val dfaB = SimpleDFA(0, mapOf(0 via Symbol.C to 1), mapOf(1 to Production(Symbol.B, AlgebraicRegex.atomic(Symbol.C))))
+        val dfaC = SimpleDFA(0, mapOf(0 via Symbol.X to 1), mapOf(0 to Production(Symbol.C, AlgebraicRegex.atomic(Symbol.X))))
+        val automata = mapOf(Symbol.A to dfaA, Symbol.B to dfaB, Symbol.C to dfaC)
+        val nullable =
+            setOf(
+                Pair(1, dfaA),
+                Pair(1, dfaB),
+                Pair(1, dfaC),
+            )
+        val first =
+            mapOf<DFAStateReference<Int, Symbol, Production<Symbol>>, Set<Symbol>>(
+                Pair(0, dfaA) to setOf(Symbol.B, Symbol.C, Symbol.X),
+                Pair(1, dfaA) to emptySet(),
+                Pair(0, dfaB) to setOf(Symbol.C, Symbol.X),
+                Pair(1, dfaB) to emptySet(),
+                Pair(0, dfaC) to setOf(Symbol.X),
+                Pair(1, dfaC) to emptySet(),
+            )
+        val follow =
+            mapOf<DFAStateReference<Int, Symbol, Production<Symbol>>, Set<Symbol>>(
+                Pair(0, dfaA) to emptySet(),
+                Pair(1, dfaA) to emptySet(),
+                Pair(0, dfaB) to emptySet(),
+                Pair(1, dfaB) to emptySet(),
+                Pair(0, dfaC) to emptySet(),
+                Pair(1, dfaC) to emptySet(),
+            )
+
+        val analyzedGrammar = AnalyzedGrammar(listOf(), automata, nullable, first, follow)
+
+        assertDoesNotThrow {
+            LLOneParser.fromAnalyzedGrammar(analyzedGrammar)
+        }
     }
 
     @Test
     fun `constructor throws given ambiguous grammar`() {
 //        A -> B | C
 //        B -> C
+//        C -> X
         val dfaA =
             SimpleDFA(
                 0,
@@ -33,38 +85,97 @@ class LLOneParserTest {
                 ),
             )
         val dfaB = SimpleDFA(0, mapOf(0 via Symbol.C to 1), mapOf(1 to Production(Symbol.B, AlgebraicRegex.atomic(Symbol.C))))
-        val dfaC = SimpleDFA(0, productions<Int, Symbol>(), mapOf(0 to Production(Symbol.C, AlgebraicRegex.atomic(Symbol.C))))
-        // ^ can we do an empty AlgebraicRegex?
+        val dfaC = SimpleDFA(0, mapOf(0 via Symbol.X to 1), mapOf(0 to Production(Symbol.C, AlgebraicRegex.atomic(Symbol.X))))
         val automata = mapOf(Symbol.A to dfaA, Symbol.B to dfaB, Symbol.C to dfaC)
         val nullable =
             setOf(
                 Pair(1, dfaA),
                 Pair(2, dfaA),
-                Pair(0, dfaC),
+                Pair(1, dfaB),
+                Pair(1, dfaC),
             )
         val first =
-            mapOf(
-                Pair(0, dfaA) to setOf(Symbol.B, Symbol.C),
+            mapOf<DFAStateReference<Int, Symbol, Production<Symbol>>, Set<Symbol>>(
+                Pair(0, dfaA) to setOf(Symbol.B, Symbol.C, Symbol.X),
                 Pair(1, dfaA) to emptySet(),
                 Pair(2, dfaA) to emptySet(),
-                Pair(0, dfaB) to setOf(Symbol.C),
+                Pair(0, dfaB) to setOf(Symbol.C, Symbol.X),
                 Pair(1, dfaB) to emptySet(),
-                Pair(0, dfaC) to emptySet(),
+                Pair(0, dfaC) to setOf(Symbol.X),
+                Pair(1, dfaC) to emptySet(),
             )
         val follow =
-            mapOf<Pair<Int, SimpleDFA<Int, Symbol, Production<Symbol>>>, Set<Symbol>>(
+            mapOf<DFAStateReference<Int, Symbol, Production<Symbol>>, Set<Symbol>>(
                 Pair(0, dfaA) to emptySet(),
                 Pair(1, dfaA) to emptySet(),
                 Pair(2, dfaA) to emptySet(),
                 Pair(0, dfaB) to emptySet(),
                 Pair(1, dfaB) to emptySet(),
                 Pair(0, dfaC) to emptySet(),
+                Pair(1, dfaC) to emptySet(),
             )
 
-        // TODO: pls help with type mismatch :<
         val analyzedGrammar = AnalyzedGrammar(listOf(), automata, nullable, first, follow)
 
         assertThrows(ParserConstructorErrorException::class.java) {
+            LLOneParser.fromAnalyzedGrammar(analyzedGrammar)
+        }
+    }
+
+    @Test
+    fun `constructor doesn't throw on a grammar with nonempty follow`() {
+//        A -> Bx
+//        B -> y | eps
+        val dfaA =
+            SimpleDFA(
+                0,
+                mapOf(0 via Symbol.B to 1, 1 via Symbol.X to 2),
+                mapOf(
+                    2 to
+                        Production(
+                            Symbol.A,
+                            AlgebraicRegex.ConcatenationRegex(AlgebraicRegex.atomic(Symbol.B), AlgebraicRegex.atomic(Symbol.X)),
+                        ),
+                ),
+            )
+        val dfaB =
+            SimpleDFA(
+                0,
+                mapOf(0 via Symbol.Y to 1),
+                mapOf(
+                    0 to Production(Symbol.B, AlgebraicRegex.atomic(Symbol.B)),
+                    // ^ this one should be empty, we cannot do that now
+                    1 to Production(Symbol.B, AlgebraicRegex.atomic(Symbol.Y)),
+                ),
+            )
+
+        val automata = mapOf(Symbol.A to dfaA, Symbol.B to dfaB)
+        val nullable =
+            setOf(
+                Pair(2, dfaA),
+                Pair(0, dfaB),
+                Pair(1, dfaB),
+            )
+        val first =
+            mapOf<DFAStateReference<Int, Symbol, Production<Symbol>>, Set<Symbol>>(
+                Pair(0, dfaA) to setOf(Symbol.X, Symbol.Y),
+                Pair(1, dfaA) to setOf(Symbol.X),
+                Pair(2, dfaA) to emptySet(),
+                Pair(0, dfaB) to setOf(Symbol.Y),
+                Pair(1, dfaB) to emptySet(),
+            )
+        val follow =
+            mapOf<DFAStateReference<Int, Symbol, Production<Symbol>>, Set<Symbol>>(
+                Pair(0, dfaA) to emptySet(),
+                Pair(1, dfaA) to emptySet(),
+                Pair(2, dfaA) to emptySet(),
+                Pair(0, dfaB) to setOf(Symbol.X),
+                Pair(1, dfaB) to setOf(Symbol.X),
+            )
+
+        val analyzedGrammar = AnalyzedGrammar(listOf(), automata, nullable, first, follow)
+
+        assertDoesNotThrow {
             LLOneParser.fromAnalyzedGrammar(analyzedGrammar)
         }
     }
