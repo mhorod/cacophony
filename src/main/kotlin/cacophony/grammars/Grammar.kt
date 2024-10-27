dd71ed34efcf2045771468c42543d8bb363582d5
Kacper Topolski <kacpertopolski@op.pl>
2024-10-27 16:40:55 +0100

    Revert accidental changes in Grammar.kt

--- src/main/kotlin/cacophony/grammars/Grammar.kt
+++ src/main/kotlin/cacophony/grammars/Grammar.kt
@@ -1,7 +1,5 @@
 package cacophony.grammars
 
-import cacophony.automata.joinAutomata
-import cacophony.automata.minimalization.buildDFAFromRegex
 import cacophony.utils.AlgebraicRegex
 
 data class Production<SymbolType>(
@@ -14,14 +12,4 @@ infix fun <SymbolType> SymbolType.produces(regex: AlgebraicRegex<SymbolType>) = 
 data class Grammar<SymbolType>(
     val start: SymbolType,
     val productions: Collection<Production<SymbolType>>,
-) {
-    fun buildAutomata() = productions.groupBy { it.lhs }.mapValues { buildAutomaton(it.value) }
-
-    private fun buildAutomaton(productions: List<Production<SymbolType>>) =
-        joinAutomata(productions.map { buildDFAFromRegex(it.rhs) }.zip(productions))
-}
-
-fun <SymbolType> grammarOf(
-    start: SymbolType,
-    vararg productions: Production<SymbolType>,
-) = Grammar(start, productions.toList())
+)
