aaca5b60abe16482face37e9d45867add11490c7
Kacper Topolski <66226018+KacperTopolski@users.noreply.github.com>
2024-10-28 15:03:38 +0100

    Find first (#66)

--- src/main/kotlin/cacophony/grammars/FindFirst.kt
+++ src/main/kotlin/cacophony/grammars/FindFirst.kt
@@ -1,10 +1,62 @@
 package cacophony.grammars
 
 import cacophony.automata.DFA
+import cacophony.utils.getProperTransitiveClosure
+
+data class GeneralizedSymbol<StateType, SymbolType, ResultType>(
+    val state: DFAStateReference<StateType, SymbolType, ResultType>?,
+    val symbol: SymbolType?,
+) {
+    init {
+        if (state == null && symbol == null) {
+            throw IllegalArgumentException("State and Symbol cannot both be nulls")
+        }
+    }
+
+    override fun toString(): String {
+        return (symbol ?: state).toString()
+    }
+}
 
 fun <StateType, SymbolType, ResultType> findFirst(
     automata: Map<SymbolType, DFA<StateType, SymbolType, ResultType>>,
-    nullable: Collection<DFAStateReference<StateType, SymbolType, ResultType>>,
+    nullable: Set<DFAStateReference<StateType, SymbolType, ResultType>>,
 ): StateToSymbolsMap<StateType, SymbolType, ResultType> {
-    TODO("Not implemented")
+    val stateReferenceToSymbol = automata.map { (symbol, dfa) -> Pair(Pair(dfa.getStartingState(), dfa), symbol) }.toMap()
+    val symbolToStateReference = stateReferenceToSymbol.map { (u, v) -> Pair(v, u) }.toMap()
+
+    // this is here for implicit access to function parameters
+    fun fromSymbol(symbol: SymbolType): GeneralizedSymbol<StateType, SymbolType, ResultType> {
+        return GeneralizedSymbol(symbolToStateReference[symbol], symbol)
+    }
+
+    fun fromState(state: DFAStateReference<StateType, SymbolType, ResultType>): GeneralizedSymbol<StateType, SymbolType, ResultType> {
+        return GeneralizedSymbol(state, stateReferenceToSymbol[state])
+    }
+
+    val graph =
+        automata
+            .values
+            .flatMap { dfa -> dfa.getAllStates().map { DFAStateReference(it, dfa) } }
+            .map { fromState(it) }
+            .associateWith { mutableSetOf<GeneralizedSymbol<StateType, SymbolType, ResultType>>() }
+
+    for (dfa in automata.values) {
+        for (production in dfa.getProductions()) {
+            val from = fromState(Pair(production.key.first, dfa))
+            val by = fromSymbol(production.key.second)
+            val to = fromState(Pair(production.value, dfa))
+
+            graph[from]!!.add(by)
+            if (by.state in nullable) {
+                graph[from]!!.add(to)
+            }
+        }
+    }
+
+    return getProperTransitiveClosure(graph).mapNotNull { (key, values) ->
+        val newKey = key.state ?: return@mapNotNull null
+        val newValues = values.mapNotNull { it.symbol }.toSet()
+        return@mapNotNull Pair(newKey, newValues)
+    }.toMap()
 }
---
+++ src/main/kotlin/cacophony/utils/GraphUtils.kt
@@ -0,0 +1,78 @@
+package cacophony.utils
+
+import kotlin.math.min
+
+// Returns a set of all elements reachable from a collection of nodes in a directed graph.
+fun <VType> getReachableFrom(
+    from: Collection<VType>,
+    graph: Map<VType, Collection<VType>>,
+): Set<VType> {
+    val reachable = from.toMutableSet()
+    val workList = from.toMutableList()
+    while (workList.isNotEmpty()) {
+        val u = workList.removeLast()
+        for (v in graph.getOrDefault(u, listOf())) {
+            if (!reachable.contains(v)) {
+                reachable.add(v)
+                workList.add(v)
+            }
+        }
+    }
+    return reachable
+}
+
+// Returns a transitive closure of a directed graph.
+fun <VType> getTransitiveClosure(graph: Map<VType, Collection<VType>>): Map<VType, Set<VType>> {
+    val closure: MutableMap<VType, Set<VType>> = mutableMapOf()
+    for (component in getStronglyConnectedComponents(graph)) {
+        val reachable = component union component.flatMap { graph[it] ?: emptyList() }.flatMap { closure[it] ?: emptyList() }
+        component.forEach { closure[it] = reachable }
+    }
+    return closure
+}
+
+// Returns a "proper" transitive closure of a directed graph (i.e. ignoring paths of length zero).
+fun <VType> getProperTransitiveClosure(graph: Map<VType, Collection<VType>>): Map<VType, Set<VType>> {
+    val closure: MutableMap<VType, Set<VType>> = mutableMapOf()
+    for (component in getStronglyConnectedComponents(graph)) {
+        val step = component.flatMap { graph[it] ?: emptyList() }
+        val reachable = step union step.flatMap { closure[it] ?: emptyList() }
+        component.forEach { closure[it] = reachable }
+    }
+    return closure
+}
+
+// Returns a list of strongly connected components of a graph in a reverse topological order.
+fun <VType> getStronglyConnectedComponents(graph: Map<VType, Collection<VType>>): List<List<VType>> {
+    val components: MutableList<List<VType>> = mutableListOf()
+    val componentIndex: MutableMap<VType, Int> = mutableMapOf()
+    val sizeMap: MutableMap<VType, Int> = mutableMapOf()
+    val stack: MutableList<VType> = mutableListOf()
+
+    val dfs =
+        DeepRecursiveFunction { v ->
+            var low = stack.size
+            sizeMap[v] = stack.size
+            stack.add(v)
+            for (e in graph[v] ?: emptyList())
+                if (componentIndex[e] == null) {
+                    low = min(low, sizeMap.getOrElse(e) { callRecursive(e) })
+                }
+            if (low == sizeMap[v]) {
+                val component: MutableList<VType> = mutableListOf()
+                while (stack.size > sizeMap[v]!!) {
+                    val u = stack.removeLast()
+                    componentIndex[u] = components.size
+                    component.add(u)
+                }
+                components.add(component)
+            }
+            low
+        }
+
+    graph.keys.forEach { v ->
+        if (sizeMap[v] == null) dfs(v)
+    }
+
+    return components
+}
