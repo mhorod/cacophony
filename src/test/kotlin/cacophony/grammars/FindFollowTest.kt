f686cba20f762c5c3602405ccfab33f34947d8e1
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-10-28 09:51:47 +0100

    More findFollow tests

--- src/test/kotlin/cacophony/grammars/FindFollowTest.kt
+++ src/test/kotlin/cacophony/grammars/FindFollowTest.kt
@@ -12,10 +12,9 @@ class FindFollowTest {
     private fun <StateType, AtomType> productions() = mapOf<Pair<StateType, AtomType>, StateType>()
 
     private fun <StateType, SymbolType, ResultType> stateToSymbols(
-        vararg results: Pair<Pair<StateType, DFA<StateType, SymbolType, ResultType>>, Set<SymbolType>>
+        vararg results: Pair<Pair<StateType, DFA<StateType, SymbolType, ResultType>>, Set<SymbolType>>,
     ) = results.associate { it }
 
-    // test find follow for simple grammar without regexes
     @Test
     fun `findFollow finds empty follow sets when there are no non-terminals`() {
         // given
@@ -28,8 +27,13 @@ class FindFollowTest {
 
         // A -> ab
         val automata = mapOf('A' to dfaA)
-        val nullable = setOf(Pair(2, dfaA)) // only accepting state is nullable
-        val first = stateToSymbols(Pair(0, dfaA) to setOf('a'), Pair(1, dfaA) to setOf('b'))
+        val nullable = setOf(Pair(2, dfaA)) // accepting state
+        val first =
+            stateToSymbols(
+                // direct productions
+                Pair(0, dfaA) to setOf('a'),
+                Pair(1, dfaA) to setOf('b'),
+            )
 
         // when
         val follow = findFollow(automata, nullable, first)
@@ -68,12 +72,16 @@ class FindFollowTest {
         val nullable = setOf(Pair(2, dfaA), Pair(2, dfaB)) // accepting states
         val first =
             stateToSymbols(
-                Pair(0, dfaA) to setOf('B', 'a'),
-                Pair(1, dfaA) to setOf('b'),
-                Pair(2, dfaA) to emptySet(),
-                Pair(0, dfaB) to setOf('c'),
-                Pair(1, dfaB) to setOf('d'),
-                Pair(2, dfaB) to emptySet(),
+                Pair(0, dfaA) to
+                    setOf(
+                        'B', // direct production
+                        'c', // transitive from B -> cd
+                    ),
+                Pair(1, dfaA) to setOf('b'), // direct production
+                Pair(2, dfaA) to emptySet(), // accepting state
+                Pair(0, dfaB) to setOf('c'), // direct production
+                Pair(1, dfaB) to setOf('d'), // direct production
+                Pair(2, dfaB) to emptySet(), // accepting state
             )
 
         // when
@@ -83,6 +91,7 @@ class FindFollowTest {
         assertThat(follow)
             .containsExactlyInAnyOrderEntriesOf(
                 stateToSymbols(
+                    // A does not appear in any production
                     Pair(0, dfaA) to emptySet(),
                     Pair(1, dfaA) to emptySet(),
                     Pair(2, dfaA) to emptySet(),
@@ -90,6 +99,163 @@ class FindFollowTest {
                     Pair(0, dfaB) to setOf('b'),
                     Pair(1, dfaB) to setOf('b'),
                     Pair(2, dfaB) to setOf('b'),
+                ),
+            )
+    }
+
+    @Test
+    fun `findFollow finds first symbols from following non-terminal`() {
+        // given
+        val dfaA =
+            SimpleDFA(
+                0,
+                mapOf(0 via 'a' to 1),
+                results(1),
+            )
+
+        val dfaB =
+            SimpleDFA(
+                0,
+                mapOf(0 via 'b' to 1, 0 via 'c' to 1),
+                results(1),
+            )
+
+        val dfaC =
+            SimpleDFA(
+                0,
+                mapOf(0 via 'A' to 1, 1 via 'B' to 2),
+                results(2),
+            )
+
+        // A -> a
+        // B -> b | c
+        // C -> AB
+        val automata = mapOf('A' to dfaA, 'B' to dfaB, 'C' to dfaC)
+        val nullable = setOf(Pair(1, dfaA), Pair(1, dfaB), Pair(2, dfaC)) // accepting states
+        val first =
+            stateToSymbols(
+                Pair(0, dfaA) to setOf('a'), // direct production
+                Pair(1, dfaA) to emptySet(), // accepting state
+                Pair(0, dfaB) to setOf('b', 'c'), // direct productions
+                Pair(1, dfaB) to emptySet(), // accepting state
+                Pair(0, dfaC) to
+                    setOf(
+                        'A', // direct production
+                        'a', // transitive from A -> a
+                    ),
+                Pair(1, dfaC) to
+                    setOf(
+                        'B', // direct production
+                        'b', // transitive from B -> b
+                    ),
+                Pair(2, dfaC) to emptySet(), // accepting state
+            )
+
+        // when
+        val follow = findFollow(automata, nullable, first)
+
+        // then
+        assertThat(follow)
+            .containsExactlyInAnyOrderEntriesOf(
+                stateToSymbols(
+                    // C does not appear in any production
+                    Pair(0, dfaC) to emptySet(),
+                    Pair(1, dfaC) to emptySet(),
+                    Pair(2, dfaC) to emptySet(),
+                    // B appears only at the end of C -> AB
+                    Pair(0, dfaB) to emptySet(),
+                    Pair(1, dfaB) to emptySet(),
+                    Pair(0, dfaA) to
+                        setOf(
+                            'B', // direct production from C -> AB
+                            'b', // transitive from B -> b
+                            'c', // transitive from B -> c
+                        ),
+                    Pair(1, dfaA) to
+                        setOf(
+                            'B', // direct production from C -> AB
+                            'b', // transitive from B -> b
+                            'c', // transitive from B -> c
+                        ),
+                ),
+            )
+    }
+
+    @Test
+    fun `findFollow finds symbols following nullable non-terminal`() {
+        // given
+        val dfaA = SimpleDFA(0, productions<Int, Char>(), results(0))
+        val dfaB =
+            SimpleDFA(
+                0,
+                mapOf(
+                    0 via 'A' to 1,
+                    1 via 'b' to 2,
+                ),
+                results(2),
+            )
+        val dfaC = SimpleDFA(0, mapOf(0 via 'c' to 1), results(1))
+        val dfaD = SimpleDFA(0, mapOf(0 via 'C' to 1, 1 via 'B' to 2), results(2))
+
+        // A -> ε
+        // B -> Ab
+        // C -> c
+        // D -> CB
+        val automata = mapOf('A' to dfaA, 'B' to dfaB, 'C' to dfaC, 'D' to dfaD)
+        val nullable = setOf(Pair(0, dfaA), Pair(2, dfaB), Pair(1, dfaC), Pair(2, dfaD)) // accepting states
+        val first =
+            stateToSymbols(
+                Pair(0, dfaA) to emptySet(), // accepting state
+                Pair(0, dfaB) to setOf('A'), // direct production
+                Pair(1, dfaB) to setOf('b'), // direct production
+                Pair(2, dfaB) to emptySet(), // accepting state
+                Pair(0, dfaC) to setOf('c'), // direct production
+                Pair(1, dfaC) to emptySet(), // accepting state
+                Pair(0, dfaD) to
+                    setOf(
+                        'C', // direct production
+                        'c', // transitive from C -> c
+                    ),
+                Pair(1, dfaD) to
+                    setOf(
+                        'B', // direct production
+                        'A', // transitive from B -> A
+                        'b', // transitive from B -> Ab with nullable A
+                    ),
+                Pair(2, dfaD) to emptySet(), // accepting state
+            )
+
+        // when
+        val follow = findFollow(automata, nullable, first)
+
+        // then
+        assertThat(follow)
+            .containsExactlyInAnyOrderEntriesOf(
+                stateToSymbols(
+                    // D does not appear in any production
+                    Pair(0, dfaD) to emptySet(),
+                    Pair(1, dfaD) to emptySet(),
+                    Pair(2, dfaD) to emptySet(),
+                    Pair(0, dfaC) to
+                        setOf(
+                            'B', // direct production from D -> CB,
+                            'A', // transitive from B -> A
+                            'b', // transitive from B -> Ab with nullable A
+                        ),
+                    Pair(1, dfaC) to
+                        setOf(
+                            'B', // direct production from D -> CB,
+                            'A', // transitive from B -> A
+                            'b', // transitive from B -> Ab with nullable A
+                        ),
+                    // B appears only at the end of D -> CB
+                    Pair(0, dfaB) to emptySet(),
+                    Pair(1, dfaB) to emptySet(),
+                    Pair(2, dfaB) to emptySet(),
+                    Pair(0, dfaA) to
+                        setOf(
+                            'b', // direct production from B -> Ab
+                        ),
                 ),
             )
     }
