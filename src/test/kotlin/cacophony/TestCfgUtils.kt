e7a8f247edc3c5d388b5990bd7504da6ac1526fa
Michal Horodecki <michalhorodecki2002@gmail.com>
2024-11-17 00:38:34 +0100

    Add tests for operators

--- src/test/kotlin/cacophony/TestCfgUtils.kt
+++ src/test/kotlin/cacophony/TestCfgUtils.kt
@@ -19,6 +19,45 @@ fun integer(value: Int) = CFGNode.Constant(value)
 
 fun registerUse(register: Register) = CFGNode.RegisterUse(register)
 
+// arithmetic
+infix fun CFGNode.add(other: CFGNode) = CFGNode.Addition(this, other)
+
+infix fun CFGNode.sub(other: CFGNode) = CFGNode.Subtraction(this, other)
+
+infix fun CFGNode.mul(other: CFGNode) = CFGNode.Multiplication(this, other)
+
+infix fun CFGNode.div(other: CFGNode) = CFGNode.Division(this, other)
+
+infix fun CFGNode.mod(other: CFGNode) = CFGNode.Modulo(this, other)
+
+fun minus(node: CFGNode) = CFGNode.Minus(node)
+
+// assignment
+infix fun CFGNode.LValue.addeq(other: CFGNode) = CFGNode.AdditionAssignment(this, other)
+
+infix fun CFGNode.LValue.subeq(other: CFGNode) = CFGNode.SubtractionAssignment(this, other)
+
+infix fun CFGNode.LValue.muleq(other: CFGNode) = CFGNode.MultiplicationAssignment(this, other)
+
+infix fun CFGNode.LValue.diveq(other: CFGNode) = CFGNode.DivisionAssignment(this, other)
+
+infix fun CFGNode.LValue.modeq(other: CFGNode) = CFGNode.ModuloAssignment(this, other)
+
+// logical
+infix fun CFGNode.eq(other: CFGNode) = CFGNode.Equals(this, other)
+
+infix fun CFGNode.neq(other: CFGNode) = CFGNode.NotEquals(this, other)
+
+infix fun CFGNode.lt(other: CFGNode) = CFGNode.Less(this, other)
+
+infix fun CFGNode.leq(other: CFGNode) = CFGNode.LessEqual(this, other)
+
+infix fun CFGNode.gt(other: CFGNode) = CFGNode.Greater(this, other)
+
+infix fun CFGNode.geq(other: CFGNode) = CFGNode.GreaterEqual(this, other)
+
+fun not(node: CFGNode) = CFGNode.LogicalNot(node)
+
 fun writeRegister(
     register: Register,
     value: CFGNode,
