6c8bf56f17a7dac4ffdaa8a4397dea29115f94ff
Jakub Oskwarek <52744180+honvex-cool@users.noreply.github.com>
2024-11-22 23:25:27 +0100

    Add interface for specifying jump condition (#154)
    * Add interface for specifying jump condition
    
    * Add missing parameter to `makeInstance`

--- src/main/kotlin/cacophony/codegen/instructions/InstructionCovering.kt
+++ src/main/kotlin/cacophony/codegen/instructions/InstructionCovering.kt
@@ -6,5 +6,5 @@ import cacophony.controlflow.CFGNode
 interface InstructionCovering {
     fun coverWithInstructions(node: CFGNode): List<Instruction>
 
-    fun coverWithInstructionsAndJump(node: CFGNode, label: BlockLabel): List<Instruction>
+    fun coverWithInstructionsAndJump(node: CFGNode, label: BlockLabel, jumpIf: Boolean = true): List<Instruction>
 }
