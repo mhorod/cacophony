65b5747b4dcf252384b997954acac0a9a525f715
Tomasz Mazur <47872060+AHGIJMKLKKZNPJKQR@users.noreply.github.com>
2024-11-24 17:03:49 +0100

    Add linearization

--- src/main/kotlin/cacophony/codegen/linearization/Linearization.kt
+++ src/main/kotlin/cacophony/codegen/linearization/Linearization.kt
@@ -1,10 +1,128 @@
 package cacophony.codegen.linearization
 
+import cacophony.codegen.BlockLabel
+import cacophony.codegen.instructions.Instruction
 import cacophony.codegen.instructions.InstructionCovering
 import cacophony.controlflow.CFGFragment
+import cacophony.controlflow.CFGLabel
+import cacophony.controlflow.CFGNode
+import cacophony.controlflow.CFGVertex
 
 typealias LoweredCFGFragment = List<BasicBlock>
 
+private typealias PartialLinearization = Pair<MutableBasicBlock, Iterator<MutableBasicBlock>>
+
+private class MutableBasicBlock(
+    val label: BlockLabel,
+    val instructions: MutableList<Instruction> = mutableListOf(),
+    val successors: MutableSet<MutableBasicBlock> = mutableSetOf(),
+    val predecessors: MutableSet<MutableBasicBlock> = mutableSetOf(),
+) : BasicBlock {
+    override fun label() = label
+
+    override fun instructions() = instructions
+
+    override fun successors() = successors
+
+    override fun predecessors() = predecessors
+}
+
+private fun <T> combine(vararg iters: Iterator<T>) = iterator { iters.forEach { yieldAll(it) } }
+
+private fun swapCondition(node: CFGNode) = CFGNode.LogicalNot(node)
+
+private class Linearizer(val fragment: CFGFragment, val covering: InstructionCovering) {
+    val visited = mutableMapOf<CFGLabel, MutableBasicBlock>()
+    var blocks = 0
+
+    fun getLabel() = "bb${blocks++}"
+
+    fun dfs(label: CFGLabel): PartialLinearization {
+        val vertex = fragment.vertices[label]!!
+        val block = MutableBasicBlock(BlockLabel(getLabel()))
+        visited[label] = block
+
+        fun handle(vertex: CFGVertex.Final) =
+            Pair(
+                block.also { it.instructions += covering.coverWithInstructions(vertex.tree) },
+                iterator { yield(block) },
+            )
+
+        fun handle(vertex: CFGVertex.Jump): PartialLinearization {
+            var result = iterator<MutableBasicBlock> { yield(block) }
+
+            (
+                visited[vertex.destination] ?: dfs(vertex.destination).let { (neighbor, iter) ->
+                    result = combine(result, iter)
+                    neighbor
+                }
+            ).also { neighbor ->
+                block.successors.add(neighbor)
+                neighbor.predecessors.add(block)
+                block.instructions += covering.coverWithInstructionsAndJump(vertex.tree, neighbor.label)
+            }
+
+            return Pair(block, result)
+        }
+
+        fun handle(vertex: CFGVertex.Conditional): PartialLinearization {
+            var result = iterator<MutableBasicBlock> { yield(block) }
+
+            var swapped = false
+            var doubled = false
+
+            val falseBlock =
+                (
+                    visited[vertex.falseDestination]?.also { swapped = true } ?: dfs(vertex.falseDestination).let { (neighbor, iter) ->
+                        result = combine(result, iter)
+                        neighbor
+                    }
+                ).also { neighbor ->
+                    block.successors.add(neighbor)
+                    neighbor.predecessors.add(block)
+                }
+
+            val trueBlock =
+                (
+                    visited[vertex.trueDestination]?.also {
+                        doubled = swapped
+                        swapped = false
+                    }
+                        ?: dfs(vertex.trueDestination).let { (neighbor, iter) ->
+                            result = combine(result, iter)
+                            neighbor
+                        }
+                ).also { neighbor ->
+                    block.successors.add(neighbor)
+                    neighbor.predecessors.add(block)
+                }
+
+            if (swapped) {
+                // false edge goes backwards, true edge forwards
+                block.instructions += covering.coverWithInstructionsAndJump(CFGNode.LogicalNot(vertex.tree), falseBlock.label)
+            } else if (doubled) {
+                // both edges go backwards, need 2 jumps
+                block.instructions += covering.coverWithInstructionsAndJump(vertex.tree, trueBlock.label)
+                block.instructions += covering.coverWithInstructionsAndJump(CFGNode.Constant(42), falseBlock.label)
+            } else {
+                // false edge goes forwards
+                block.instructions += covering.coverWithInstructionsAndJump(vertex.tree, trueBlock.label)
+            }
+
+            return Pair(block, result)
+        }
+
+        return when (vertex) {
+            is CFGVertex.Final -> handle(vertex)
+            is CFGVertex.Jump -> handle(vertex)
+            is CFGVertex.Conditional -> handle(vertex)
+        }
+    }
+}
+
 fun linearize(fragment: CFGFragment, covering: InstructionCovering): LoweredCFGFragment {
-    TODO("Not yet implemented")
+    val bbList = mutableListOf<MutableBasicBlock>()
+    Linearizer(fragment, covering).dfs(fragment.initialLabel).second.forEach { bbList.add(it) }
+
+    return bbList
 }
