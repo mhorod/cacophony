c2b9cbba0fca94fdac2078d3850874b6ffaa9707
Michał Horodecki <43152679+mhorod@users.noreply.github.com>
2024-11-25 11:06:17 +0100

    Add Instructions to Patterns (#159)
    * half of the boilerplate is done
    
    * Apply review
    
    * Make template fields protected

--- src/main/kotlin/cacophony/codegen/patterns/cacophonyPatterns/SideEffectPatterns.kt
+++ src/main/kotlin/cacophony/codegen/patterns/cacophonyPatterns/SideEffectPatterns.kt
@@ -1,6 +1,5 @@
 package cacophony.codegen.patterns.cacophonyPatterns
 
-import cacophony.codegen.instructions.Instruction
 import cacophony.codegen.patterns.SideEffectPattern
 import cacophony.codegen.patterns.SlotFill
 import cacophony.controlflow.*
@@ -27,89 +26,121 @@ val sideEffectPatterns =
 object AdditionAssignmentRegisterPattern : SideEffectPattern, RegisterAssignmentTemplate() {
     override val tree = lhsSlot addeq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            add(reg(lhsRegisterLabel), reg(rhsLabel))
+        }
 }
 
 object AdditionAssignmentMemoryPattern : SideEffectPattern, MemoryAssignmentTemplate() {
     override val tree = lhsSlot addeq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            val temporaryRegister = Register.VirtualRegister()
+            mov(temporaryRegister, mem(reg(lhsLabel)))
+            add(temporaryRegister, reg(rhsLabel))
+            mov(mem(reg(lhsLabel)), temporaryRegister)
+        }
 }
 
 object SubtractionAssignmentRegisterPattern : SideEffectPattern, RegisterAssignmentTemplate() {
     override val tree = lhsSlot subeq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            sub(reg(lhsRegisterLabel), reg(rhsLabel))
+        }
 }
 
 object SubtractionAssignmentMemoryPattern : SideEffectPattern, MemoryAssignmentTemplate() {
     override val tree = lhsSlot subeq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            val temporaryRegister = Register.VirtualRegister()
+            mov(temporaryRegister, mem(reg(lhsLabel)))
+            sub(temporaryRegister, reg(rhsLabel))
+            mov(mem(reg(lhsLabel)), temporaryRegister)
+        }
 }
 
 object MultiplicationAssignmentRegisterPattern : SideEffectPattern, RegisterAssignmentTemplate() {
     override val tree = lhsSlot muleq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            imul(reg(lhsRegisterLabel), reg(rhsLabel))
+        }
 }
 
 object MultiplicationAssignmentMemoryPattern : SideEffectPattern, MemoryAssignmentTemplate() {
     override val tree = lhsSlot muleq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            val temporaryRegister = Register.VirtualRegister()
+            mov(temporaryRegister, mem(reg(lhsLabel)))
+            imul(temporaryRegister, reg(rhsLabel))
+            mov(mem(reg(lhsLabel)), temporaryRegister)
+        }
 }
 
 object DivisionAssignmentRegisterPattern : SideEffectPattern, RegisterAssignmentTemplate() {
     override val tree = lhsSlot diveq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            mov(rax, reg(lhsRegisterLabel))
+            cqo()
+            idiv(reg(rhsLabel))
+            mov(reg(lhsRegisterLabel), rax)
+        }
 }
 
 object DivisionAssignmentMemoryPattern : SideEffectPattern, MemoryAssignmentTemplate() {
     override val tree = lhsSlot diveq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            mov(rax, mem(reg(lhsLabel)))
+            cqo()
+            idiv(reg(rhsLabel))
+            mov(mem(reg(lhsLabel)), rax)
+        }
 }
 
 object ModuloAssignmentRegisterPattern : SideEffectPattern, RegisterAssignmentTemplate() {
     override val tree = lhsSlot modeq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            mov(rax, reg(lhsRegisterLabel))
+            cqo()
+            idiv(reg(rhsLabel))
+            mov(reg(lhsRegisterLabel), rdx)
+        }
 }
 
 object ModuloAssignmentMemoryPattern : SideEffectPattern, MemoryAssignmentTemplate() {
     override val tree = lhsSlot modeq rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            mov(rax, mem(reg(lhsLabel)))
+            cqo()
+            idiv(reg(rhsLabel))
+            mov(mem(reg(lhsLabel)), rdx)
+        }
 }
 
 object ReturnPattern : SideEffectPattern {
     override val tree = CFGNode.Return
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            ret()
+        }
 }
 
 object PushPattern : SideEffectPattern {
@@ -117,9 +148,10 @@ object PushPattern : SideEffectPattern {
 
     override val tree = CFGNode.Push(CFGNode.ValueSlot(childLabel))
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            push(reg(childLabel))
+        }
 }
 
 object PopPattern : SideEffectPattern {
@@ -127,23 +159,28 @@ object PopPattern : SideEffectPattern {
 
     override val tree = CFGNode.Pop(CFGNode.RegisterSlot(regLabel))
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            pop(reg(regLabel))
+        }
 }
 
 object RegisterAssignmentPattern : SideEffectPattern, RegisterAssignmentTemplate() {
     override val tree = lhsSlot assign rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            mov(reg(lhsRegisterLabel), reg(rhsLabel))
+        }
 }
 
 object MemoryAssignmentPattern : SideEffectPattern, MemoryAssignmentTemplate() {
     override val tree = lhsSlot assign rhsSlot
 
-    override fun makeInstance(fill: SlotFill): List<Instruction> {
-        TODO("Not yet implemented")
-    }
+    override fun makeInstance(fill: SlotFill) =
+        instructions(fill) {
+            val temporaryRegister = Register.VirtualRegister()
+            mov(temporaryRegister, reg(rhsLabel))
+            mov(mem(reg(lhsLabel)), temporaryRegister)
+        }
 }
--- src/main/kotlin/cacophony/controlflow/HardwareRegister.kt
+++ src/main/kotlin/cacophony/controlflow/HardwareRegister.kt
@@ -19,6 +19,32 @@ enum class HardwareRegister(val isCallPreserved: Boolean) {
     R15(true),
 }
 
+enum class HardwareRegisterByte(private val hardwareRegister: HardwareRegister) {
+    AL(HardwareRegister.RAX),
+    BL(HardwareRegister.RBX),
+    CL(HardwareRegister.RCX),
+    DL(HardwareRegister.RDX),
+    SIL(HardwareRegister.RSI),
+    DIL(HardwareRegister.RDI),
+    SPL(HardwareRegister.RSP),
+    BPL(HardwareRegister.RBP),
+    R8B(HardwareRegister.R8),
+    R9B(HardwareRegister.R9),
+    R10B(HardwareRegister.R10),
+    R11B(HardwareRegister.R11),
+    R12B(HardwareRegister.R12),
+    R13B(HardwareRegister.R13),
+    R14B(HardwareRegister.R14),
+    R15B(HardwareRegister.R15),
+    ;
+
+    companion object {
+        fun fromRegister(register: HardwareRegister): HardwareRegisterByte {
+            return entries.find { it.hardwareRegister == register } ?: error("No byte register for $register")
+        }
+    }
+}
+
 val REGISTER_ARGUMENT_ORDER =
     listOf(
         HardwareRegister.RDI,
