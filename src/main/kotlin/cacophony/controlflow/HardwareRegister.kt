8bed1a62fe3e6aaab1c5df4d8b58672502f116e5
jjaworska <51903947+jjaworska@users.noreply.github.com>
2024-11-26 00:03:18 +0100

    Add instructions (#162)
    * half of the boilerplate is done
    
    * First couple of instructions
    
    * initial commit
    
    * Organized file structure
    
    * required changes
    
    ---------
    
    Co-authored-by: Michal Horodecki <michalhorodecki2002@gmail.com>

--- src/main/kotlin/cacophony/controlflow/HardwareRegister.kt
+++ src/main/kotlin/cacophony/controlflow/HardwareRegister.kt
@@ -1,5 +1,7 @@
 package cacophony.controlflow
 
+import java.util.*
+
 enum class HardwareRegister(val isCallPreserved: Boolean) {
     RAX(false),
     RBX(true),
@@ -16,7 +18,9 @@ enum class HardwareRegister(val isCallPreserved: Boolean) {
     R12(true),
     R13(true),
     R14(true),
-    R15(true),
+    R15(true), ;
+
+    override fun toString(): String = name
 }
 
 enum class HardwareRegisterByte(private val hardwareRegister: HardwareRegister) {
@@ -38,10 +42,11 @@ enum class HardwareRegisterByte(private val hardwareRegister: HardwareRegister) 
     R15B(HardwareRegister.R15),
     ;
 
+    override fun toString(): String = name
+
     companion object {
-        fun fromRegister(register: HardwareRegister): HardwareRegisterByte {
-            return entries.find { it.hardwareRegister == register } ?: error("No byte register for $register")
-        }
+        fun fromRegister(register: HardwareRegister): HardwareRegisterByte =
+            entries.find { it.hardwareRegister == register } ?: error("No byte register for $register")
     }
 }
 
