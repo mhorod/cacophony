dc9b1c2bcd2745764b9da04317937e877e5c59bd
Katzper Michno <katzper.michno@gmail.com>
2024-12-15 15:57:51 +0100

    add unit tests for RegistersInteraction

--- src/test/kotlin/cacophony/codegen/registers/RegistersInteractionTest.kt
+++ src/test/kotlin/cacophony/codegen/registers/RegistersInteractionTest.kt
@@ -7,11 +7,9 @@ import cacophony.controlflow.Register
 import io.mockk.every
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-@Disabled
 class RegistersInteractionTest {
     private fun mockInstruction(def: Set<Register>, use: Set<Register>): Instruction {
         val instruction = mockk<Instruction>()
@@ -24,6 +22,8 @@ class RegistersInteractionTest {
         val instruction = mockk<CopyInstruction>()
         every { instruction.registersRead } returns use
         every { instruction.registersWritten } returns def
+        every { instruction.copyInto() } returns def.first()
+        every { instruction.copyFrom() } returns use.first()
         return instruction
     }
 
@@ -38,10 +38,10 @@ class RegistersInteractionTest {
     //             2: [def B, use B/def C]
     //            /                       \
     //  1: [def A]                         4: [use A]
-    //            \         --------------/
-    //             3: [def D]
+    //            \                       /
+    //             3: [def D] -----------/
     @Test
-    fun `properly calculates registersInteraction without copy instructions`() {
+    fun `properly calculates interference without copy instructions`() {
         // given
         val regA = Register.VirtualRegister()
         val regB = Register.VirtualRegister()
@@ -104,7 +104,7 @@ class RegistersInteractionTest {
 
     //  1: [def A] --> 2(copy): [def B/use A] --> 3: [use A]
     @Test
-    fun `recognizes when registers interfere only in copy instructions`() {
+    fun `properly founds copy`() {
         // given
         val regA = Register.VirtualRegister()
         val regB = Register.VirtualRegister()
@@ -149,9 +149,56 @@ class RegistersInteractionTest {
         )
     }
 
+    //  1: [def A] --> 2(copy): [def B/use A] --> 3: [def B/useA]
+    @Test
+    fun `recognizes that copy is not created if copyInto is defined more than once`() {
+        // given
+        val regA = Register.VirtualRegister()
+        val regB = Register.VirtualRegister()
+
+        val block3 =
+            mockBlock(
+                listOf(mockInstruction(setOf(regB), setOf(regA))),
+                setOf(),
+            )
+        val block2 =
+            mockBlock(
+                listOf(mockCopyInstruction(setOf(regB), setOf(regA))),
+                setOf(block3),
+            )
+
+        val block1 =
+            mockBlock(
+                listOf(mockInstruction(setOf(regA), setOf())),
+                setOf(block2),
+            )
+
+        every { block3.predecessors() } returns setOf(block2)
+        every { block2.predecessors() } returns setOf(block1)
+        every { block1.predecessors() } returns setOf()
+
+        // when
+        val registersInteraction = analyzeRegistersInteraction(listOf(block1, block2, block3))
+
+        // then
+        assertThat(registersInteraction.allRegisters).containsExactlyInAnyOrder(regA, regB)
+        assertThat(registersInteraction.interference).containsExactlyInAnyOrderEntriesOf(
+            mapOf(
+                regA to setOf(regB),
+                regB to setOf(regA),
+            ),
+        )
+        assertThat(registersInteraction.copying).containsExactlyInAnyOrderEntriesOf(
+            mapOf(
+                regA to setOf(),
+                regB to setOf(),
+            ),
+        )
+    }
+
     //  1: [def A] --> 2(copy): [def B/use A] --> 3: [def A] --> 4: [use B]
     @Test
-    fun `recognizes when copied registers interfere in other non copy instructions`() {
+    fun `recognizes that copy is not created if copyFrom is defined again in lifetime of copyInto`() {
         // given
         val regA = Register.VirtualRegister()
         val regB = Register.VirtualRegister()
@@ -171,6 +218,7 @@ class RegistersInteractionTest {
                 listOf(mockCopyInstruction(setOf(regB), setOf(regA))),
                 setOf(block3),
             )
+
         val block1 =
             mockBlock(
                 listOf(mockInstruction(setOf(regA), setOf())),
